c16d1dbf8c235c5d74346d8b213cb0b890cc8206
Aman Sultan Baig <amansultanbaig@gmail.com>
2023-05-14 18:23:03 +0500

    sequelize connection established

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const connectDatabase = require("./db/connection");
 const expressApplication = require("./experssApp");
 const app = express();
 require("dotenv").config();
@@ -6,6 +7,7 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const startApplication = () => {
+  connectDatabase(process.env.DATABASE_NAME, process.env.DB_USER);
   expressApplication(app);
   app.listen(port, () => console.log("Application is running"));
 };
