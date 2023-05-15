fb5cc032786939e234810e30a32e8729dd4f7a66
Aman Sultan Baig <amansultanbaig@gmail.com>
2023-05-15 22:54:43 +0500

    sequelize connection established

--- experssApp.js
+++ experssApp.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const schoolController = require("./controllers/school-controller");
 
 const expressApplication = (app) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use("/school", schoolController);
 };
 
 module.exports = expressApplication;
